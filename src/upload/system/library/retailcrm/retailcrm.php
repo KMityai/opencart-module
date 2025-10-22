73251d0ef491790d2001d417bf209787231ead84
Dmitriy <48244883+KMityai@users.noreply.github.com>
2025-10-22 14:59:02 +0300

    retailcrm.php changed version

--- src/upload/system/library/retailcrm/retailcrm.php
+++ src/upload/system/library/retailcrm/retailcrm.php
@@ -17,7 +17,7 @@ class Retailcrm {
 
     const RETAILCRM_DISCOUNT = 'retailcrm_discount';
     const RETAILCRM_DISCOUNT_SORT_ORDER = 8;
-    const VERSION_MODULE = '4.2.2';
+    const VERSION_MODULE = '4.2.3';
 
     protected $registry;
 
